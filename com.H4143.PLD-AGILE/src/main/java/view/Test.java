2fa0de1f593a954cc3433ac9482eeb9c73c9422b
CharlesXu0715 <xuyuantaoxyt@gmail.com>
2021-11-22 09:23:44 +0100

    Test

---
+++ com.H4143.PLD-AGILE/src/main/java/view/Test.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.List;
+import model.*;
+
+public class Test{
+    public static void main(String[] args) {
+        String filemap="./fichiersXML2020/smallMap.xml";
+        String filerequest="./fichiersXML2020/requestsSmall1.xml";
+        FileLoader l=new FileLoader();
+        List<Intersection> intersections=l.loadIntersection(filemap);
+        List<Road> roads=l.loadRoad(filemap);
+        RequestList requests=l.loadRequest(filerequest);
+        System.out.println(intersections.size());
+        System.out.println(roads.size());
+        System.out.println(requests.getRequests().size());
+        for (Intersection i:intersections) {
+            System.out.println(i.getId());
+        }
+        for (Road i:roads) {
+            System.out.println(i.getName());
+        }
+        for (Request i:requests.getRequests()) {
+            System.out.println(i.getPickPoint().getDuration());
+
+        }
+    }
+}
