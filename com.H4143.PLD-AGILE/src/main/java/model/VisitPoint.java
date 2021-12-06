4158ed5858b1c0635d3affb0ae63a8500251f87e
transontung00 <74469691+transontung00@users.noreply.github.com>
2021-12-06 09:53:34 +0100

    Update VisitPoint.java

--- com.H4143.PLD-AGILE/src/main/java/model/VisitPoint.java
+++ com.H4143.PLD-AGILE/src/main/java/model/VisitPoint.java
@@ -1,21 +1,18 @@
 package model;
 
-import java.util.Observable;
 
-@SuppressWarnings("deprecation")
-public class VisitPoint extends Observable{
+public class VisitPoint {
 //	private String pointId; //id of intersection
-	private boolean selected;
 	private Intersection intersection;
     //type 0 = depot point, type 1 = pickup point, type 2 = delivery point
 	private int type;
 	private int duration;
+	private int order = 0;
 
 	public VisitPoint(Intersection intersection,int duration, int type) {
     	super();
         this.intersection=intersection;
         this.duration=duration;
-        this.selected=false;
         this.type = type;
     }
 
@@ -23,13 +20,6 @@ public class VisitPoint extends Observable{
 //        return pointId;
 //    }
 
-    public boolean isSelected() {
-		return selected;
-	}
-
-	public void setSelected(boolean selected) {
-		this.selected = selected;
-	}
 
 	public int getDuration(){
         return duration;
@@ -50,6 +40,14 @@ public class VisitPoint extends Observable{
 		ans+="  duration : "+duration+"\n";
 		ans+="  point : "+intersection+"\n";
 		return ans;
+	}
+
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
 	}
 	
 //	public void setPointIndex(int pointIndex) {
