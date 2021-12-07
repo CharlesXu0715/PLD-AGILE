dffa0d269c7c2a42e664491a6aa88e1705772948
GenT322 <74548275+GenT322@users.noreply.github.com>
2021-12-07 15:41:39 +0100

    Update TestTSP

--- com.H4143.PLD-AGILE/src/main/java/tsp/TestTSP.java
+++ com.H4143.PLD-AGILE/src/main/java/tsp/TestTSP.java
@@ -1,66 +1,35 @@
 package tsp;
 
-import java.util.List;
-import java.util.Map;
-
-import model.Intersection;
+import model.CityMap;
+import model.Model;
 import model.Request;
 import model.RequestList;
-import model.Segment;
 import model.VisitPoint;
 import singleton.XMLFileLoader;
 
 public class TestTSP {
 
 	public static void main(String[] args) {
-		testLoad();
+//		testLoad();
 //		testAddVisitPoint();
+		testAddVisitPointToIndex();
 //		testRemoveVisitPoint();
 //		testChangeOrder();
 	}
 	
 	public static void testLoad() {
-//		Intersection iA = new Intersection(0,0,"A",0);
-//		Intersection iB = new Intersection(0,0,"B",1);
-//		Intersection iC = new Intersection(0,0,"C",2);
-//		Intersection iD = new Intersection(0,0,"D",3);
-//		iA.setIndex(0);
-//		iB.setIndex(1);
-//		iC.setIndex(2);
-//		iD.setIndex(3);
-//		Road rAB = new Road(0,1,"AB",2);
-//		Road rBC = new Road(1,2,"BC",3);
-//		Road rCD = new Road(2,3,"CD",4);
-//		Road rDA = new Road(3,0,"DA",5);
-//		Road rAC = new Road(0,2,"AC",6);
-//		List<Intersection> intersections = new ArrayList<Intersection>();
-//		List<Road> roads = new ArrayList<Road>();
-//		intersections.add(iA);
-//		intersections.add(iB);
-//		intersections.add(iC);
-//		intersections.add(iD);
-//		roads.add(rAB);
-//		roads.add(rBC);
-//		roads.add(rCD);
-//		roads.add(rDA);
-//		roads.add(rAC);
-//		Request r1 = new Request(1,1,iC,iD);
-//		List<Request> listR = new ArrayList<Request>();
-//		listR.add(r1);
-//		RequestList requestList = new RequestList("8.0","A",listR);
-//		CityMap cityMap = new CityMap(roads,intersections);
-//		requestList.setDepartIndex(0);
 		TSP tsp = new TSP1();
-//		String mapName = "./target/test-classes/testMap.xml";
-//		String requestName = "./target/test-classes/testRequests.xml";
-		String mapName = "src/main/resources/largeMap.xml";
-		String requestName = "src/main/resources/requestsLarge9.xml";
-		XMLFileLoader fileLoader = new XMLFileLoader();
-		fileLoader.loadMap(mapName);
-		List<Intersection> intersections=fileLoader.getIntersections();
-        List<Segment> roads=fileLoader.getRoads();
-		Map cityMap = new Map(roads,intersections);
-		RequestList requestList = fileLoader.loadRequest(requestName);
+		CityMap cityMap=null;
+		RequestList requestList=null;
+		Model model=new Model();
+		try {
+			cityMap = XMLFileLoader.getInstance().loadMap(null);
+			model.setMap(cityMap);
+			requestList = XMLFileLoader.getInstance().loadRequest(null, model);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Graph g = new ShortestPathGraph(requestList,cityMap);
 		long startTime = System.currentTimeMillis();
 		tsp.searchSolution(10000, g);
@@ -74,14 +43,17 @@ public class TestTSP {
 
 	public static void testAddVisitPoint() {
 		TSP tsp = new TSP1();
-		String mapName = "src/main/resources/smallMap.xml";
-		String requestName = "src/main/resources/requestsSmall2.xml";
-		XMLFileLoader fileLoader = new XMLFileLoader();
-		fileLoader.loadMap(mapName);
-		List<Intersection> intersections=fileLoader.getIntersections();
-        List<Segment> roads=fileLoader.getRoads();
-		Map cityMap = new Map(roads,intersections);
-		RequestList requestList = fileLoader.loadRequest(requestName);
+		CityMap cityMap=null;
+		RequestList requestList=null;
+		Model model=new Model();
+		try {
+			cityMap = XMLFileLoader.getInstance().loadMap(null);
+			model.setMap(cityMap);
+			requestList = XMLFileLoader.getInstance().loadRequest(null, model);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Graph g = new ShortestPathGraph(requestList,cityMap);
 		tsp.searchSolution(20000, g);
 		System.out.println("----------\r\nBefore:\r\n----------");
@@ -96,8 +68,50 @@ public class TestTSP {
 		System.out.print("Solution of cost "+tsp.getRoute().getDuration()+" found in "
 				+(System.currentTimeMillis() - startTime)+"ms : ");
 		System.out.println(tsp.getRoute());
-		Request newRequest = new Request(200,100,intersections.get(10),intersections.get(11));
+		Request newRequest = new Request(200,100,cityMap.getIntersections().get(10),cityMap.getIntersections().get(11));
 		tsp.addNewRequest(newRequest);
+		System.out.println("----------\r\nAfter:\r\n----------");
+//		for (int i=0;i<g.getNbVertices();i++) {
+//			for (int j=0;j<g.getNbVertices();j++) {
+//				if (i!=j)
+////				System.out.println("From: "+g.getPath(i, j).getStart().getId()+" to: "+g.getPath(i, j).getEnd().getId());
+//				System.out.println(g.getPath(i, j));
+//			}
+//		}
+		System.out.print("Solution of cost "+tsp.getRoute().getDuration()+" found in "
+				+(System.currentTimeMillis() - startTime)+"ms : ");
+		System.out.println(tsp.getRoute());
+	}
+	
+	public static void testAddVisitPointToIndex() {
+		TSP tsp = new TSP1();
+		CityMap cityMap=null;
+		RequestList requestList=null;
+		Model model=new Model();
+		try {
+			cityMap = XMLFileLoader.getInstance().loadMap(null);
+			model.setMap(cityMap);
+			requestList = XMLFileLoader.getInstance().loadRequest(null, model);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Graph g = new ShortestPathGraph(requestList,cityMap);
+		tsp.searchSolution(20000, g);
+		System.out.println("----------\r\nBefore:\r\n----------");
+//		for (int i=0;i<g.getNbVertices();i++) {
+//			for (int j=0;j<g.getNbVertices();j++) {
+//				if (i!=j)
+////				System.out.println("From: "+g.getPath(i, j).getStart().getId()+" to: "+g.getPath(i, j).getEnd().getId());
+//				System.out.println(g.getPath(i, j));
+//			}
+//		}
+		long startTime = System.currentTimeMillis();
+		System.out.print("Solution of cost "+tsp.getRoute().getDuration()+" found in "
+				+(System.currentTimeMillis() - startTime)+"ms : ");
+		System.out.println(tsp.getRoute());
+		Request newRequest = new Request(200,100,cityMap.getIntersections().get(10),cityMap.getIntersections().get(11));
+		tsp.addRequestToIndex(newRequest, 1, 3);
 		System.out.println("----------\r\nAfter:\r\n----------");
 //		for (int i=0;i<g.getNbVertices();i++) {
 //			for (int j=0;j<g.getNbVertices();j++) {
@@ -113,14 +127,17 @@ public class TestTSP {
 	
 	public static void testRemoveVisitPoint() {
 		TSP tsp = new TSP1();
-		String mapName = "src/main/resources/smallMap.xml";
-		String requestName = "src/main/resources/requestsSmall2.xml";
-		XMLFileLoader fileLoader = new XMLFileLoader();
-		fileLoader.loadMap(mapName);
-		List<Intersection> intersections=fileLoader.getIntersections();
-        List<Segment> roads=fileLoader.getRoads();
-		Map cityMap = new Map(roads,intersections);
-		RequestList requestList = fileLoader.loadRequest(requestName);
+		CityMap cityMap=null;
+		RequestList requestList=null;
+		Model model=new Model();
+		try {
+			cityMap = XMLFileLoader.getInstance().loadMap(null);
+			model.setMap(cityMap);
+			requestList = XMLFileLoader.getInstance().loadRequest(null, model);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Graph g = new ShortestPathGraph(requestList,cityMap);
 		tsp.searchSolution(20000, g);
 		System.out.println("----------\r\nBefore:\r\n----------");
@@ -152,16 +169,17 @@ public class TestTSP {
 
 	public static void testChangeOrder() {
 		TSP tsp = new TSP1();
-		String mapName = "src/main/resources/smallMap.xml";
-		String requestName = "src/main/resources/requestsSmall2.xml";
-		XMLFileLoader fileLoader = new XMLFileLoader();
-		fileLoader.loadMap(mapName);
-		List<Intersection> intersections=fileLoader.getIntersections();
-        List<Segment> roads=fileLoader.getRoads();
-		Map cityMap = new Map(roads,intersections);
-		RequestList requestList = fileLoader.loadRequest(requestName);
-		
-		
+		CityMap cityMap=null;
+		RequestList requestList=null;
+		Model model=new Model();
+		try {
+			cityMap = XMLFileLoader.getInstance().loadMap(null);
+			model.setMap(cityMap);
+			requestList = XMLFileLoader.getInstance().loadRequest(null, model);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Graph g = new ShortestPathGraph(requestList,cityMap);
 		tsp.searchSolution(20000, g);
 		System.out.println("----------\r\nBefore:\r\n----------");
